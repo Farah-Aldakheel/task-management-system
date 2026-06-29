79bdd12fe7cde1f1cba5cff58dc916fe0830dbb0
farah <farahaldakheel2020@gmail.com>
2026-06-29 09:45:41 +0300

    create basic task managment system

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print("Hello, World!")
+tasks = []
+
+while True:
+    print ("\n==== tasks management system ====")
+    print ("1. Add task")
+    print ("2. View tasks")
+    print ("3. exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        task = input("enter your task: ")
+        tasks.append(task)
+
+    elif choice == "2":
+        if len(tasks) == 0:
+            print("no tasks available ")
+        else:
+            print ("your tasks are: ")
+            for i, task in enumerate(tasks,start=0):
+                print (f"{i+1}. {task}")
+
+    elif choice == "3":
+        print ("exiting the program")
+        break
+
+    else:
+        print("invalid choice,please try againg")
+1
